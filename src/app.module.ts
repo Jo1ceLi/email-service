bf50a39badbc35d4a193124443481833846e5450
Jo1ceLi <ms0615122@gmail.com>
2022-06-29 00:43:26 +0800

    feat: add joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // validationSchema: Joi.object({
+      //   SENDGRID_API_KEY: Joi.required(),
+      //   RABBITMQ_URL: Joi.required(),
+      //   RABBITMQ_EMAIL_QUEUE: Joi.required(),
+      // }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
